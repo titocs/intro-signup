ec507b262d9620a9d8246634dd0b0c18168f7365
Hfn <55591161+titocs@users.noreply.github.com>
2022-01-06 13:17:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -18,11 +18,13 @@ button.addEventListener("click", function(){
                 error[i].style.display = "block";
                 input[i].style.backgroundSize = "20px";
                 input[i].style.border = "1px solid hsl(0, 77%, 59%)";
+                input[i].style.color = "hsl(0, 77%, 59%)";
             }
             else{
                 error[i].style.display = "none";
                 input[i].style.backgroundSize = "0";
                 input[i].style.border = "1px solid rgba(139, 139, 139, 0.5)";
+                input[i].style.color = "var(--Dark-Blue)";
             }
         }
         else{
@@ -30,11 +32,13 @@ button.addEventListener("click", function(){
                 error[i].style.display = "block";
                 input[i].style.backgroundSize = "20px";
                 input[i].style.border = "1px solid hsl(0, 77%, 59%)";
+                input[i].style.color = "hsl(0, 77%, 59%)";
             }
             else{
                 error[i].style.display = "none";
                 input[i].style.backgroundSize = "0";
                 input[i].style.border = "1px solid rgba(139, 139, 139, 0.5)";
+                input[i].style.color = "var(--Dark-Blue)";
             }
         }
     }
